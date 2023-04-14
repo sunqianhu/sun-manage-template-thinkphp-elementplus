f2adf386a17c2202868d4103886fe45916e0551d
469661426 <469661426@qq.com>
2023-04-14 17:04:00 +0800

    增加多应用模式

---
+++ server/app/admin/controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\admin\controller;
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        return 'index';
+    }
+
+    public function hello()
+    {
+        return 'hello';
+    }
+
+    public function test()
+    {
+        return 'test';
+    }
+}
