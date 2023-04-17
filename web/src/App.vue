082f4b6f239b56bf79dda349138bcf59dc91287d
469661426 <469661426@qq.com>
2023-04-17 10:42:58 +0800

    增加elementplus包

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="zhCn">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<script setup></script>
+<script setup>
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+</script>
 
 <style scoped></style>
--- web/src/views/Test.vue
+++ web/src/views/Test.vue
@@ -1,4 +1,16 @@
-<template><div>test</div></template>
+<template>
+  <div>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    ddddd
+  </div>
+</template>
 
 <script setup>
 import { loginApi } from "../api/login";
