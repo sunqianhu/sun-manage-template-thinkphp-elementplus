e4cbec5e02bae38699929625549dafe19cc06c60
469661426 <469661426@qq.com>
2023-04-17 12:09:34 +0800

    增加sass，增加样式变量定义

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/Login.vue")
+    },
+    {
       path: "/test",
       name: "test",
       component: () => import("../views/Test.vue")
---
+++ web/src/views/Login.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="page-login">
+    <div class="main">
+      <div class="header">
+        <div class="title">
+          <img src="@/assets/image/logo.svg" class="logo" alt="logo" />
+          <span class="name">管理后台</span>
+        </div>
+        <div class="desc">基于vue的一个渐进式组件化管理后台</div>
+      </div>
+      <el-form :model="form" :rules="rules" ref="formRef" class="form">
+        <el-form-item prop="account">
+          <el-input
+            :prefix-icon="User"
+            placeholder="请输入账号"
+            size="large"
+            v-model="form.account"
+          />
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            placeholder="请输入密码"
+            show-password
+            size="large"
+            :prefix-icon="Lock"
+            v-model="form.password"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="large" @click="onSubmit" class="button">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="footer">
+      <div class="browser">浏览器支持：Edge ≥ 79 Firefox≥78 Chrome≥64 Safari≥12</div>
+      <div class="copyright">Copyright &copy; 2023 asun</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const form = ref({});
+const formRef = ref(null);
+const rules = {
+  account: [{ required: true, message: "请输入账号", trigger: "blur" }],
+  password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+};
+
+// 提交
+const onSubmit = () => {
+  formRef.value.validate(async (valid) => {
+    if (!valid) {
+      return;
+    }
+
+    const res = await axios.post("/auth/login", form.value);
+    if (res.code != 1) {
+      ElMessage({
+        message: res.message,
+        type: "warning"
+      });
+      return;
+    }
+
+    // 存储
+    store.commit("setToken", res.data.token);
+    store.commit("setExpiresIn", res.data.expires_in);
+
+    // 导航
+    router.push("/home");
+  });
+};
+</script>
+
+<style lang="scss">
+html {
+  height: 100%;
+  body {
+    height: 100%;
+    #app {
+      height: 100%;
+    }
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.page-login {
+  height: 100%;
+  background: #f0f2f5 url(@/assets/images/login/background.svg) no-repeat 50%;
+  background-size: 100%;
+  position: relative;
+
+  .main {
+    width: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: 2;
+    margin-left: -175px;
+    margin-top: -200px;
+    .header {
+      text-align: center;
+      .title {
+        line-height: 44px;
+        .logo {
+          height: 44px;
+          vertical-align: top;
+          margin-right: 16px;
+        }
+        .name {
+          font-size: 30px;
+        }
+      }
+      .desc {
+        font-size: 14px;
+        color: rgba(0, 0, 0, 0.45);
+        margin-top: 12px;
+        margin-bottom: 40px;
+      }
+    }
+
+    .form {
+      margin-top: 30px;
+      .button {
+        width: 100%;
+      }
+    }
+  }
+
+  .footer {
+    position: absolute;
+    width: 100%;
+    bottom: 10px;
+    z-index: 1;
+    text-align: center;
+    font-size: 12px;
+    color: #919191;
+  }
+}
+</style>
