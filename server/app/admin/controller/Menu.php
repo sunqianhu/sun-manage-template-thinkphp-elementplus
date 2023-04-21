35192cbbd03540d470f8d97d487b18b65cd3f879
469661426 <469661426@qq.com>
2023-04-21 15:56:32 +0800

    增加路由菜单接口

---
+++ server/app/admin/controller/Menu.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\admin\controller;
+
+/**
+ * 菜单
+ */
+class Menu extends Base
+{
+    /**
+     * 得到菜单
+     * @return void
+     */
+    public function getMenus()
+    {
+
+    }
+
+}
---
+++ server/app/admin/controller/Route.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\admin\controller;
+
+/**
+ * 路由
+ */
+class Route extends Base
+{
+    /**
+     * 获取路由
+     * @return void
+     */
+    public function getRoutes()
+    {
+
+    }
+
+}
