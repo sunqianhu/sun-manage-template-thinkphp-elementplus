8b9c2d4152442b96df6d358cd73ec0084bea385f
469661426 <469661426@qq.com>
2023-09-22 11:26:47 +0800

    项目域名目录修改

--- www/front/config.js
+++ www/front/config.js
@@ -1,6 +1,6 @@
 // 开发环境
 export const config = {
   proxyPrefix: "/api",
-  server: "http://tpvuemanage.sunqianhu123.cc",
+  server: "http://thinkphpvuemanage.sunqianhu123.cc",
   messageWebSocketServerUrl: "ws://127.0.0.1:9001"
 };
