9dd4d1f9f5d2c44a694271b6c1cb5fe49c664143
sunqianhu <469661426@qq.com>
2023-10-07 14:27:09 +0800

    登录页面优化

--- www/front/src/view/Login.vue
+++ www/front/src/view/Login.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="login">
-    <div class="wrap">
+    <div class="background">
+      <video src="@/asset/image/login/background.mp4" autoplay loop muted></video>
+    </div>
+    <div class="form-wrap">
       <div class="header">
         <div class="title">
           <img src="@/asset/image/logo.png" class="logo" alt="logo" />
-          <span class="name">通用后台管理系统</span>
+          <span class="name">督查系统</span>
         </div>
-        <div class="subtitle">使用thinkphp vue element plus开发的通用后台管理系统</div>
       </div>
       <el-form :model="login" :rules="rules" ref="loginRef" class="form" @keyup.enter="submitForm">
         <el-form-item prop="account">
@@ -56,7 +58,6 @@
         </el-form-item>
       </el-form>
     </div>
-
     <div class="footer">
       <div class="browser">浏览器支持：Edge ≥ 79 Firefox≥78 Chrome≥64 Safari≥12</div>
       <div class="copyright">Copyright &copy; 2023 asun</div>
@@ -148,48 +149,51 @@ html {
 .login {
   height: 100%;
   background-color: #f0f2f5;
-  background-image: url(@/asset/image/login/background.svg);
-  background-repeat: no-repeat;
-  background-size: 100%;
   position: relative;
-
-  .wrap {
-    padding: 16px;
+  .background {
+    position: absolute;
     left: 0px;
     right: 0px;
+    top: 0px;
+    bottom: 0px;
+    z-index: 1;
+    video {
+      width: 100%;
+      height: 100%;
+      object-fit: fill;
+    }
+  }
+  .form-wrap {
+    padding: 32px;
     position: absolute;
-    z-index: 2;
+    left: 50%;
     top: 50%;
-    margin-top: -200px;
-
+    transform: translate(-50%, -50%);
+    z-index: 3;
+    background: rgba(255, 255, 255, 0.9);
+    border-radius: var(--border-radius-big);
+    box-shadow: 0px 0px 32px rgba(0, 0, 0, 0.12);
     .header {
       text-align: center;
       .title {
         line-height: 44px;
         .logo {
-          height: 44px;
-          vertical-align: top;
+          height: 30px;
+          vertical-align: -4px;
         }
         .name {
-          margin-left: 16px;
-          font-size: 30px;
+          margin-left: 12px;
+          font-size: 24px;
           font-weight: bold;
         }
-      }
-      .subtitle {
-        color: var(--font-color-light);
-        margin-top: 12px;
       }
     }
 
     .form {
-      width: 350px;
-      margin-top: 50px;
-      margin-left: auto;
-      margin-right: auto;
+      margin-top: 30px;
       .captcha {
         .input {
-          width: 200px;
+          width: 170px;
         }
         img {
           margin-left: 16px;
@@ -212,7 +216,7 @@ html {
     left: 16px;
     right: 16px;
     bottom: 16px;
-    z-index: 1;
+    z-index: 2;
     text-align: center;
     font-size: var(--font-size-small);
     color: var(--font-color-light);
