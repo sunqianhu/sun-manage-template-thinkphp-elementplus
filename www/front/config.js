0ae9a15088f8253beb4a67d4802b3fb210f52ed6
sunqianhu <469661426@qq.com>
2023-11-27 15:13:39 +0800

    修改项目地址

--- www/front/config.js
+++ www/front/config.js
@@ -1,6 +1,6 @@
 // 开发环境
 export const config = {
   proxyPrefix: "/api",
-  proxyServer: "http://thinkphpvuemanage.sunqianhu123.cc",
+  proxyServer: "http://thinkphpvuemanagetemplate.sunqianhu123.cc",
   messageWebSocketServerUrl: "ws://127.0.0.1:9001"
 };
