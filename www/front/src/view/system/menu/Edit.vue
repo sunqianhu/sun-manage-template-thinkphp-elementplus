e8585fe16dac62eb6e1fadfe26c46e0681f9d89a
sunqianhu <469661426@qq.com>
2024-01-22 17:36:57 +0800

    修复菜单类型字段错误的问题

--- www/front/src/view/system/menu/Edit.vue
+++ www/front/src/view/system/menu/Edit.vue
@@ -104,7 +104,7 @@ const emits = defineEmits(["close", "submited"]);
 const menu = ref({});
 const menuRef = ref();
 const rules = {
-  type: [{ required: true, message: "菜单类型不能为空", trigger: "blur" }],
+  type_id: [{ required: true, message: "菜单类型不能为空", trigger: "blur" }],
   name: [{ required: true, message: "菜单名称不能为空", trigger: "blur" }],
   key: [{ required: true, message: "菜单key不能为空", trigger: "blur" }],
   sort: [{ required: true, message: "排序不能为空", trigger: "blur" }]
